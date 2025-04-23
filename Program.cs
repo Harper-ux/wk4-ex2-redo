116e07f9a5e991cc4bd06b51642676a4fddead99
Harper-ux <hrshafer23@amarillocollege.com>
2025-04-22 19:00:16 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wk4_ex2_rework
+{
+    internal class Program
+    {
+        static void assignments()
+        {
+            double assignments = (assignments(Console.ReadLine()));
+        }
+
+
+
+
+
+
+
+        static void CalculateGrade(double assignment, double midterm, double finalExam)
+        {
+            double finalGrade = (assignment * 0.4) + (midterm * 0.2) + (finalExam * 0.4);
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
